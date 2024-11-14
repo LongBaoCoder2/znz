32935258d65cbe8ae731c79e502a13856f3a590b
LongBaoCoder2 <longbao.it@gmail.com>
2024-11-14 17:53:36 +0700

    Initial project

---
+++ sfu/src/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+
+const app : Express = express();
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
+})
+
+const PORT = 8333;
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
