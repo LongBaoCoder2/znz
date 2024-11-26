844a51779268fb59e304458595d540ef0fbb22f0
CongKony <chicong442004@gmail.com>
2024-11-26 18:38:08 +0700

    feat: setup SSL

--- sfu/src/index.ts
+++ sfu/src/index.ts
@@ -1,11 +1,25 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+import https from "https";
+import fs from "fs";
+import path from "path";
 
+// Tạo ứng dụng Express
 const app: Express = express();
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world");
+
+// Đọc chứng chỉ và khóa SSL
+const key = fs.readFileSync(path.join(__dirname, "./ssl/localhost.key"), "utf-8");
+const cert = fs.readFileSync(path.join(__dirname, "./ssl/localhost.crt"), "utf-8");
+
+
+// Tạo HTTPS server
+const server = https.createServer({ key, cert }, app);
+
+// Middleware Express
+app.get("/", (req, res) => {
+  res.send("This is a secure server");
 });
 
-const PORT = 8333;
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+// Lắng nghe request
+server.listen(3001, () => {
+  console.log("listening on 3001");
 });
