28b84ba8ffb5dca1a640844b37d860d13b9bec15
Thai Van Manh <thaivanmanh2004@gmail.com>
2024-12-21 13:01:16 +0700

    feat(client-home-UI): add home screen

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import SignIn from "./routes/signin.tsx";
 import SignUp from "./routes/signup.tsx";
+import Home from "./routes/home.tsx";
+import Meeting from "./routes/meeting.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,6 +14,8 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/meeting/:URI" element={<Meeting />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
