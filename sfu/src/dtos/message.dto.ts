d34e55397ea307ff2f266568621de89698d55cfd
Long Bảo <longbao.it@gmail.com>
2024-12-21 12:33:20 +0700

    Revert "Thang/feat/message api"

--- sfu/src/dtos/message.dto.ts
+++
@@ -1,7 +0,0 @@
-export class SendMessageDto {
-    public meetingId: number;
-    public userId: number;
-    public content: string;
-    public toUser: number;
-    public isPinned: boolean;
-}
--- sfu/src/index.ts
+++ sfu/src/index.ts
@@ -7,7 +7,6 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import express, { Express } from "express";
 import homeRoute from "./routes/home.route";
-import messageRoute from "./routes/message.route";
 import { errorMiddleware, loggingMiddleware, serverErrorMiddleware, serverListenHandler } from "./middlewares/common";
 
 const app: Express = express();
@@ -29,7 +28,6 @@ app.use(errorMiddleware);
 
 /* ================= Define route ================= */
 app.use("/api", homeRoute);
-app.use("/api", messageRoute);
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, serverListenHandler(PORT));
